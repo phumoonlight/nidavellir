6cbfad3209fe79d1af29babf85b672a754692eaf
phumoonlight <phumoonlight@hotmail.com>
2022-01-07 16:20:55 +0700

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import { apiController } from './api/api'
 
 export const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api', apiController)
