1edc4a46c5575c15ab46efff9f00e8fd15c858d3
Poosarn Moolmuang <phumoonlight@hotmail.com>
2022-01-16 21:13:32 +0700

    refactor: upload function

--- src/api/upload/upload.model.ts
+++ src/api/upload/upload.model.ts
@@ -1,10 +1,34 @@
 import { Request } from 'express'
-import { uploadFile as uploadFileToStorage } from '../../firebase'
+import { firebaseBucket } from '../../firebase'
 import { MESSAGE } from '../../const'
 
 interface UploadResult {
   message: string
   uploadedUrl: string
+}
+
+const FOLDER = 'uploads'
+
+const uploadFileToStorage = async (file: Express.Multer.File): Promise<string> => {
+  return new Promise<string>((resolve) => {
+    if (!file) return resolve('')
+    const generated = `${Date.now()}-${Math.round(Math.random() * 1e9)}`
+    const splitedFileName = file.originalname.split('.')
+    const fileExtension = splitedFileName[splitedFileName.length - 1]
+    const newFileName = `u-${generated}.${fileExtension}`
+    const dest = `${FOLDER}/${newFileName}`
+    const blob = firebaseBucket.file(dest)
+    const blobStream = blob.createWriteStream({ resumable: false })
+    blobStream.on('error', (error) => {
+      console.error(error)
+      resolve('')
+    })
+    blobStream.on('finish', () => {
+      const url = `https://firebasestorage.googleapis.com/v0/b/${firebaseBucket.name}/o/${FOLDER}%2F${newFileName}?alt=media`
+      resolve(url)
+    })
+    blobStream.end(file.buffer)
+  })
 }
 
 export const initResponseData = () => ({
--- src/firebase.ts
+++ src/firebase.ts
@@ -5,41 +5,14 @@ import { ENV } from './env'
 import serviceAccount from '../service-account.json'
 
 const BUCKET = ENV.storageBucketName
-const FOLDER = 'uploads'
 
 initializeApp({
 	credential: cert(serviceAccount as ServiceAccount),
 	storageBucket: BUCKET,
 })
 
-const getUploadedUrl = (fileName: string) => {
-	return `https://firebasestorage.googleapis.com/v0/b/${BUCKET}/o/${FOLDER}%2F${fileName}?alt=media`
-}
-
 export const firestore = getFirestore()
 
 export const firebaseStorage = getStorage()
 
 export const firebaseBucket = firebaseStorage.bucket()
-
-export const uploadFile = async (file: Express.Multer.File): Promise<string> => {
-	return new Promise<string>((resolve) => {
-		if (!file) return resolve('')
-		const generated = `${Date.now()}-${Math.round(Math.random() * 1e9)}`
-		const splitedFileName = file.originalname.split('.')
-		const fileExtension = splitedFileName[splitedFileName.length - 1]
-		const newFileName = `u-${generated}.${fileExtension}`
-		const dest = `${FOLDER}/${newFileName}`
-		const blob = firebaseBucket.file(dest)
-		const blobStream = blob.createWriteStream({ resumable: false })
-		blobStream.on('error', (error) => {
-			console.error(error)
-			resolve('')
-		})
-		blobStream.on('finish', () => {
-			const url = getUploadedUrl(newFileName)
-			resolve(url)
-		})
-		blobStream.end(file.buffer)
-	})
-}
