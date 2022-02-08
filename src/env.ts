25f0def436e87bb5d7d727ee27779421e8741acf
phumoonlight <phumoonlight@hotmail.com>
2022-02-08 21:55:19 +0700

    refactor: remove unused env

--- src/env.ts
+++ src/env.ts
@@ -5,14 +5,12 @@ dotenv.config()
 const {
 	PORT = '4000',
 	STORAGE_BUCKET_NAME = '',
-	JWT_SECRET = '<secret>',
 	BOOKMARK_STORAGE_BUCKET_NAME = '',
 } = process.env
 
 export const ENV = {
 	port: +PORT,
 	storageBucketName: STORAGE_BUCKET_NAME,
-	jwtSecret: JWT_SECRET,
 	bookmark: {
 		storageBucketName: BOOKMARK_STORAGE_BUCKET_NAME,
 	},
