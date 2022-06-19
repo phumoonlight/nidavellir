18af462977871046b95e9ad058ba7f3fc79568cf
Poosarn Moolmuang <phumoonlight@hotmail.com>
2022-06-19 17:26:53 +0700

    chore: job change doc id

---
+++ src/scripts/docid.ts
@@ -0,0 +1,10 @@
+import { vurlFirebase } from '../api/vurl/vurl.firebase'
+import { LinkDocument, vurlModel } from '../api/vurl/vurl.model'
+
+vurlFirebase.firestore.collection('links').get().then((snapshot) => {
+  snapshot.docs.forEach(async (doc) => {
+    const d = doc.data()
+    await vurlModel.deleteBookmark(doc.id)
+    vurlModel.createBookmark(d.uid, d as LinkDocument)
+  })
+})
